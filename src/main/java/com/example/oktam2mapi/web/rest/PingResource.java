a181c3964caea0d6f92d7a7666907c059b64c722
Rajesh Padmakumaran <rajesh.sgr@gmail.com>
2023-04-19 15:48:43 -0500

    update API endpoint

--- src/main/java/com/example/oktam2mapi/web/rest/PingResource.java
+++ src/main/java/com/example/oktam2mapi/web/rest/PingResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/api")
+@RequestMapping("api/v1/")
 public class PingResource {
 
     private static final Logger log = LoggerFactory.getLogger(PingResource.class);
